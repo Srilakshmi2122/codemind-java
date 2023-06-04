21edc8a23cff2c656ed2fc89317254db1afba7fd
Srilakshmi2122 <119720694+Srilakshmi2122@users.noreply.github.com>
2023-06-04 21:57:05 +0530

    import java.util.Scanner; public class Sample {     public static void main(String args[])     {         char ch;         Scanner sc=new Scanner(System.in);         ch=sc.next().charAt(0);         if(ch=='V' || ch== 'v')          System.out.println("Voilet");         else if(ch=='I' || ch== 'i')          System.out.println("Indigo");          else if(ch=='B' || ch== 'b')          System.out.println("Blue");          else if(ch=='G' || ch== 'g')          System.out.println("Green");          else if(ch=='Y' || ch== 'y')          System.out.println("Yellow");         else  if(ch=='O' || ch== 'o')          System.out.println("Orange");          else if(ch=='R' || ch== 'r')          System.out.println("Red");          else          System.out.println("-1");
             sc.close();
        }
    }

--- Color_Code.java
+++ Color_Code.java
@@ -5,27 +5,24 @@ public class Sample
     {
         char ch;
         Scanner sc=new Scanner(System.in);
-        // String s=sc.next();
-        // ch=s.charAt(0);
         ch=sc.next().charAt(0);
-        if(ch=='V' || ch=='v')
-          System.out.println("Violet");
-        else if(ch=='I' || ch=='i')
-          System.out.println("Indigo");
-        else if(ch=='B' || ch=='b')
-          System.out.println("Blue");
-        else if(ch=='G' || ch=='g')
-          System.out.println("Green");
-        else if(ch=='Y' || ch=='y')
-          System.out.println("Yellow");
-        else if(ch=='O' ||ch=='o')
-          System.out.println("Orange");
-        else if(ch=='R' || ch=='r')
-          System.out.println("Red");
-        else
-          System.out.println("-1");
-        
-        sc.close();
-        
+        if(ch=='V' || ch== 'v')
+         System.out.println("Voilet");
+        else if(ch=='I' || ch== 'i')
+         System.out.println("Indigo"); 
+         else if(ch=='B' || ch== 'b')
+         System.out.println("Blue");
+         else if(ch=='G' || ch== 'g')
+         System.out.println("Green");
+         else if(ch=='Y' || ch== 'y')
+         System.out.println("Yellow");
+        else  if(ch=='O' || ch== 'o')
+         System.out.println("Orange");
+         else if(ch=='R' || ch== 'r')
+         System.out.println("Red");
+         else
+         System.out.println("-1");
+         
+         sc.close();
     }
 }
